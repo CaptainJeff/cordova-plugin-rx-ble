e1a9729f188a4578a3dc6d70e080f473fba558b5
Jeffrey Drakos <jeffreydrakos@Jeffreys-MBP.fios-router.home>
2018-01-09 16:24:40 -0500

    changing varibale names

--- www/central.js
+++ www/central.js
@@ -3,7 +3,7 @@
 var argscheck = require('cordova/argscheck');
 var exec = require('cordova/exec');
 
-var PLUGIN_NAME = 'CentralPlugin';
+var PLUGIN_NAME = 'CoachCareBluetooth';
 var getValue = argscheck.getValue;
 
 function isNotAcceptable(val) {
@@ -12,7 +12,7 @@ function isNotAcceptable(val) {
 
 var Central = {
 
-    startDeviceScan: function(options, successCallback, errorCallback) {
+    scan: function(options, successCallback, errorCallback) {
         var uuids = null;
         var args = [uuids];
         exec(successCallback, errorCallback, PLUGIN_NAME, 'startDeviceScan', args);
@@ -26,7 +26,7 @@ var Central = {
         exec(successCallback, errorCallback, PLUGIN_NAME, 'monitorDeviceDisconnect', []);
     },
 
-    connectToDevice: function(options, successCallback, errorCallback) {
+    connect: function(options, successCallback, errorCallback) {
         var deviceId = getValue(options.deviceId, undefined);
 
         if (isNotAcceptable(deviceId)) {
@@ -37,7 +37,7 @@ var Central = {
         exec(successCallback, errorCallback, PLUGIN_NAME, 'connectToDevice', args);
     },
 
-    disconnectDevice: function(options, successCallback, errorCallback) {
+    disconnect: function(options, successCallback, errorCallback) {
         var deviceId = getValue(options.deviceId, undefined);
 
         if (isNotAcceptable(deviceId)) {
